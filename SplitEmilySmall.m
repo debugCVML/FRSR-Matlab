9d1d4dc795e8d17d94d30621ed45f1609fdbec56
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-04-18 16:59:38 +0800

    write png format instead of jpg

--- SplitEmilySmall.m
+++ SplitEmilySmall.m
@@ -3,6 +3,6 @@ images = data.Emily_small;
 
 for idx =1:size(images,3)
     img = images(:,:,idx);
-    fullName = sprintf('%d.jpg',idx);
+    fullName = sprintf('%d.png',idx);
     imwrite(img, fullName);
 end
